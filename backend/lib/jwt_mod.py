6a4df6f721f9f724536c124b8805b3ef9eac12cb
jiad-saegus <jiad.abdul@saegus.com>
2024-09-06 14:12:22 +0200

    jwt secret fix

--- backend/lib/jwt_mod.py
+++ backend/lib/jwt_mod.py
@@ -1,6 +1,6 @@
 import jwt
 import os
-SECRET = os.environ.get('JWT_SECRET')
+SECRET = os.environ.get('JWT_SECRET') or "secret"
 
 def encode_jwt(payload):
     return jwt.encode(payload, SECRET, algorithm='HS256')
